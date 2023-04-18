b48c9b714a9cf7f24e3b79a04fa3429de5487188
shacharli <59819774+shacharli@users.noreply.github.com>
2023-04-18 19:17:07 +0300

    Add files via upload

--- stock2json.py
+++ stock2json.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import sys
 
+
 def main():
     symbol = sys.argv[1]
     url = 'https://www.marketwatch.com/investing/stock/' + symbol
@@ -13,8 +14,17 @@ def main():
         soup = BeautifulSoup(r.text, 'html.parser')
         
         price_coin = soup.find('h2', attrs={'class': 'intraday__price'}).text.strip("\n")
-        data["price"] = price_coin[1:].strip("\n")
-        data["currency"] = price_coin[0]
+        
+        try :
+            if price_coin[0]!="-":
+                int(price_coin[0])
+            data["price"] = price_coin
+            data["currency"] = ""
+            
+        except:
+            data["price"] = price_coin[1:].strip("\n")
+            data["currency"] = price_coin[0]
+            
         data["last updated"] = soup.find('span', attrs={'class': 'timestamp__time'}).text[14:]
         data['status']= soup.find('div', attrs={'class': 'status'}).text
         data["point_change"]= soup.find('span', attrs={'class': 'change--point--q'}).text
